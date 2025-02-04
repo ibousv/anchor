20635abdff2b1c8119444e26b6ba7f8fc0cccd15
ib <132789212+ibousv@users.noreply.github.com>
2025-02-04 17:52:49 +0000

    init sep1

---
+++ anchor_project/anchor/sep1/info.py
@@ -0,0 +1,21 @@
+
+def toml(*args):
+    return {
+        "DOCUMENTATION": {
+            "ORG_NAME": "Stellar Development Foundation",
+            "ORG_URL": "https://stellar.org",
+        },
+        "PRINCIPALS": [
+            {
+                "name": "GT team",
+            }
+        ],
+        "CURRENCIES": [
+            {
+                "code": "TEST",
+                "issuer": "GDVTYCV5F76ZGZALKSISRPSPXMIY425PJ2F5VXZE4GWWFE3MCQIPH5PJ",
+                "status": "test",
+                "display_decimals": 2,
+            }
+        ]
+    }
